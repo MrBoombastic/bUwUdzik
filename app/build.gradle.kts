7226efd3bedf95c20eee8fa78b2030ce4e5c60d6
MrBoombastic <contact@amroz.xyz>
2026-01-07 04:25:37 +0100

    chore: bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.mrboombastic.buwudzik"
         minSdk = 34
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
